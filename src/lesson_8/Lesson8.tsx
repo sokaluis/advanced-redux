3e18840e717cd5e2db4791187b8f2af929c3139a
Luis Azocar <lazocar.dev@gmail.com>
2023-02-21 13:24:29 -0300

    lesson 8 - adding components to Router

--- src/lesson_8/Lesson8.tsx
+++ src/lesson_8/Lesson8.tsx
@@ -3,12 +3,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './styles.css';
 import { store } from './app/stores';
+import Layout from './components/Layout';
+import Public from './components/Public';
 
 const App = () => {
   return (
-    <>
-      <div>Lesson8</div>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route index element={<Public />} />
+        {/* <Route path="login" element={<Login />} /> */}
+
+        {/* protected routes */}
+        {/* <Route element={<RequireAuth />}>
+          <Route path="welcome" element={<Welcome />} />
+          <Route path="userslist" element={<UsersList />} /> */}
+        {/* </Route> */}
+
+      </Route>
+    </Routes>
   );
 };
 
